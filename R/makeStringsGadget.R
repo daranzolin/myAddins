9e192b7f9cda74c318213c508d4b1c1870bb3f38
daranzolin <daranzolin@gmail.com>
2016-08-13 22:03:21 -0700

    Improved gadget w/preview

--- R/makeStringsGadget.R
+++ R/makeStringsGadget.R
@@ -1,20 +1,39 @@
 makeStringsGadget <- function(...) {
 
   ui <- miniUI::miniPage(
-    miniUI::gadgetTitleBar("Type your strings freely!"),
+    miniUI::gadgetTitleBar("makeStrings Gadget"),
     miniUI::miniContentPanel(
-      shiny::textInput("sep", "Separator:"),
-      shiny::textInput("text", "Strings:")
+      shiny::textInput("sep", "Separator:", ", "),
+      shiny::radioButtons("radio", label = "Class:", choices = list("Character" = "char",
+                                                                    "Numeric" = "num")),
+      shiny::br(),
+      shiny::p("Type here:"),
+      tags$textarea(id = "typings", rows = 3, cols = 75, "These, are, a, few, of, my, favorite, things"),
+      shiny::br(),
+      shiny::p("Preview:"),
+      shiny::verbatimTextOutput("preview")
     )
   )
 
   server <- function(input, output, session) {
 
+    output$preview <- shiny::renderPrint({
+      if (input$radio == "char") {
+        unlist(strsplit(as.character(input$typings), split = input$sep))
+      } else {
+        as.numeric(unlist(strsplit(input$typings, split = input$sep)))
+      }
+    })
+
     shiny::observeEvent(input$done, {
-      returnValue <- unlist(strsplit(as.character(input$text), split = input$sep))
-      shiny::stopApp(returnValue)
+      if (input$radio == "char") {
+        rv <- unlist(strsplit(as.character(input$typings), split = input$sep))
+      } else {
+        rv <- as.numeric(unlist(strsplit(input$typings, split = input$sep)))
+      }
+      stopApp(rv)
     })
   }
-
-  shiny::runGadget(ui, server)
+  viewer <- shiny::dialogViewer(dialogName = 'typeStrings', width = 900, height = 900)
+  shiny::runGadget(ui, server, stopOnCancel = TRUE, viewer = viewer)
 }
